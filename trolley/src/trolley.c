7a7202e142ee23e0d7991dc97aaf0d938dbeb4df
Alexandros Papadopoulos <sdi2000164@di.uoa.gr>
2024-11-07 10:30:06 +0200

    testing

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -1,49 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    long long int leftTrack, rightTrack; //long long int to fit -10^18 to 10^18
+    long long left_cost, right_cost;
 
-    // Loop continues until the user provides invalid input or reaches EOF
     while (1) {
-        // Getting the number of people on the left track with input validation
+        // Ζητά από το χρήστη το κόστος για την αριστερή επιλογή
         printf("Please enter the cost of going left: ");
-        if (scanf("%lld", &leftTrack) != 1) {
-            if (scanf("%lld", &leftTrack) == EOF){
-                        break;
-                }
-            else{
-                printf("Terminating.\n");
-                        return 1;
-                }
+        long long left_input = scanf("%lld", &left_cost);
+        if (left_input == EOF) {
+            // Τερματίζει με κωδικό εξόδου 0 αν φτάσει σε EOF για την αριστερή επιλογή
+            printf("Terminating.\n");
+            return 0;
+        } else if (left_input != 1) {
+            // Αν δεν υπάρχει έγκυρη είσοδος για το αριστερό κόστος, εμφανίζει μήνυμα και τερματίζει
+            printf("Terminating.\n");
+            return 1;
         }
 
-        // Getting the number of people on the right track with input validation
-
+        // Ζητά από το χρήστη το κόστος για την δεξιά επιλογή
         printf("Please enter the cost of going right: ");
-        if (scanf("%lld", &rightTrack) != 1) {
-            if (scanf("%lld", &rightTrack) == EOF){
-                        break;
-                }
-            else{
-                printf("Terminating.\n");
-                        return 1;
-                }
+        long long right_input = scanf("%lld", &right_cost);
+        if (right_input == EOF) {
+            // Τερματίζει με κωδικό εξόδου 0 αν φτάσει σε EOF για την δεξιά επιλογή
+            printf("No right cost provided.\n");
+            return 1;
+        } else if (right_input != 1) {
+            // Αν δεν υπάρχει έγκυρη είσοδος για το δεξί κόστος, εμφανίζει μήνυμα και τερματίζει
+            printf("Terminating.\n");
+            return 1;
         }
 
-        // Decide which track to suggest based on the number of people
-        if (leftTrack < rightTrack) {
+        // Επιλέγει την επιλογή με το μικρότερο κόστος
+        if (left_cost < right_cost) {
             printf("Go left.\n");
-        }
-        else if (rightTrack < leftTrack) {
+        } else if (right_cost < left_cost) {
             printf("Go right.\n");
-        }
-        else {
-            // If both tracks have the same number of people, choose the left track
+        } else {
+            // Αν είναι ίσα, επιλέγει "Go left"
             printf("Go left.\n");
         }
     }
-
-    printf("Terminating.\n");
-
-    return 0;
 }
